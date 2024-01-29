7322045bf8bbc45b6406394d8842df7779b981f6
LanzallamasX <142258662+LanzallamasX@users.noreply.github.com>
2024-01-29 17:42:16 -0300

    Update Faqs.jsx moved up

--- components/Faqs/Faqs.jsx
+++ components/Faqs/Faqs.jsx
@@ -21,9 +21,9 @@ const Faqs = ({}) => {
                       Si terminás de pagar tu crédito y seguís vendiendo activamente, podrás acceder a un nuevo crédito.
         `,
         event: 'credit_more_info.clicked',
+        faqUser: 'User ?',
         questionClicked: 'Como puedo obtener una oferta de PedidosYa Creditos',
         questionEvent: 'isOnline',
-        faqUser: 'User ?',
 
     },
     {
@@ -33,9 +33,9 @@ const Faqs = ({}) => {
         `Podés solicitar un crédito de PedidosYa <strong> desde el lugar en el que te encuentres. Recibirás el dinero en tu cuenta bancaria en un máximo de 48 horas hábiles</strong> posteriores a la solicitud del crédito.<br />
         <strong> La solicitud es segura, online y fácil</strong>, conoce más detalles en los <a href="https://creditos.pedidosya.com/terminos-y-condiciones" title="Términos y condiciones de PedidosYA Créditos" target="_blank"><strong>Términos y condiciones</strong></a> de PedidosYa Créditos.`,
         event: 'credit_more_info.clicked',
+        faqUser: 'User ?',
         questionClicked: 'Como es el credito que puedo solicitar',
         questionEvent: 'fixedInstallment',
-        faqUser: 'User ?',
 
     },
     {
@@ -44,9 +44,9 @@ const Faqs = ({}) => {
       content:
         `El dinero <strong> recibirás en la cuenta bancaria asociada a tu contrato con PedidosYa.</strong> La misma en la que recibes la liquidación por tus ventas semanales.`,
         event: 'credit_more_info.clicked',
+        faqUser: 'User ?',
         questionClicked: 'Donde recibo el dinero',
         questionEvent: 'moneyPlacement',
-        faqUser: 'User ?',
 
     },
     {
@@ -57,18 +57,18 @@ const Faqs = ({}) => {
         Las cuotas<strong> serán consideradas como pagas cuando se realice el cobro total</strong>. Si en la liquidación semanal de ventas no se pudo realizar el cobro total de la cuota,<strong> se descontará de la siguiente liquidación hasta alcanzar el monto total a cobrar</strong>.<br />                          
         Y<strong> si quieres completar el pago de la cuota antes del cierre de la siguiente liquidación, podrás realizar una transferencia bancaria</strong> a la cuenta indicada en la solapa Créditos, dentro de la sección Finanzas en Partner Portal.`,
         event: 'credit_more_info.clicked',
+        faqUser: 'User ?',
         questionClicked: 'Como y cuando pago las cuotas',
         questionEvent: 'howIsPaid',
-        faqUser: 'User ?',
 
     },
   ];
 
 
-  const handleFaqsClick = (event, questionClicked, questionEvent, faqUser) => {
+  const handleFaqsClick = (event, faqUser, questionClicked, questionEvent) => {
     
-    window.dataLayer.push({'event': event, 'questionClicked': questionClicked, 'questionEvent': questionEvent, 'userId': faqUser});
-              console.log({'event': event, 'questionClicked': questionClicked, 'questionEvent': questionEvent, 'userId': faqUser});    
+    window.dataLayer.push({'event': event, 'userId': faqUser, 'questionClicked': questionClicked, 'questionEvent': questionEvent});
+              console.log({'event': event, 'userId': faqUser, 'questionClicked': questionClicked, 'questionEvent': questionEvent});    
   }; 
 
   const itemClasses = {
