2dd67d6a232d893749cf9c28b0c09098f06c4182
LanzallamasX <142258662+LanzallamasX@users.noreply.github.com>
2024-01-29 16:47:25 -0300

    Update CreditRequest.jsx

--- components/CreditRequest/CreditRequest.jsx
+++ components/CreditRequest/CreditRequest.jsx
@@ -109,6 +109,9 @@ const CreditRequest = () => {
 
             <MainButton
                 color="buttonRed"
+                href={
+                  "https://pwdh.adj.st/fintech/cards/redirector?adj_t=6t2xp9f_a7g3qbs&adj_deep_link=pedidosya%3A%2F%2Ffintech%2Fcards%2Fredirector&adj_campaign=Landing&adj_adgroup=CTA&adj_creative=Header"
+                }
                 text = "Simular crédito"
                 eventClick={"credit_selection.proceeded"}
                 userid={'User ?'}
